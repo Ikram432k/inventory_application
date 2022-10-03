1e3096586d0e12547fd3cd7fea4d7563116c7fec
ikram mohideen <ikram432k@gmail.com>
2022-10-03 19:20:36 +0530

    added form for creating new anime available

--- controllers/availabilityController.js
+++ controllers/availabilityController.js
@@ -1,4 +1,6 @@
 const Availability = require("../models/availability");
+const Anime = require("../models/anime");
+const { body, validationResult } = require("express-validator");
 
 //display list of all availability
 exports.availability_list =(req,res,next)=>{
@@ -33,14 +35,71 @@ exports.availability_detail =(req,res,next)=>{
 };
 
 //display availability create form on GET
-exports.availability_create_get =(req,res)=>{
-    res.send("NOT IMPEMENTENED: availability create GET");
-};
+exports.availability_create_get =(req, res, next) => {
+    Anime.find({}, "title").exec((err, animes) => {
+      if (err) {
+        return next(err);
+      }
+      // Successful, so render.
+      res.render("availability_form", {
+        title: "Create Availability",
+        anime_list: animes,
+      });
+    });
+  };
+  
 
 //handle availability create on POST
-exports.availability_create_post =(req,res)=>{
-    res.send("NOT IMPEMENTENED: availability create POST");
-};
+exports.availability_create_post = [
+  // Validate and sanitize fields.
+  body("anime", "Anime must be specified").trim().isLength({ min: 1 }).escape(),
+  body("studio", "studio must be specified")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("status").escape(),
+
+  // Process request after validation and sanitization.
+  (req, res, next) => {
+    // Extract the validation errors from a request.
+    const errors = validationResult(req);
+
+    // Create a availability object with escaped and trimmed data.
+    const availability = new Availability({
+      anime: req.body.anime,
+      studio: req.body.studio,
+      status: req.body.status,
+    });
+
+    if (!errors.isEmpty()) {
+      // There are errors. Render form again with sanitized values and error messages.
+      Anime.find({}, "title").exec(function (err, animes) {
+        if (err) {
+          return next(err);
+        }
+        // Successful, so render.
+        res.render("availability_form", {
+          title: "Create Availability",
+          anime_list: animes,
+          selected_book: availability.anime._id,
+          errors: errors.array(),
+          availability,
+        });
+      });
+      return;
+    }
+
+    // Data from form is valid.
+    availability.save((err) => {
+      if (err) {
+        return next(err);
+      }
+      // Successful: redirect to new record.
+      res.redirect(availability.url);
+    });
+  },
+];
+
 
 //display availability delete form on GET
 exports.availability_delete_get =(req,res)=>{
