581bcb4d0623809c5cc0a3a8e8a6177de621ac40
ikram mohideen <ikram432k@gmail.com>
2022-10-03 17:49:26 +0530

    added form for new animeCreator

--- controllers/creatorContollers.js
+++ controllers/creatorContollers.js
@@ -1,6 +1,7 @@
 const AnimeCreator = require("../models/animeCreator");
 const async = require("async");
 const Anime = require("../models/anime");
+const { body, validationResult } = require("express-validator");
 
 //display list of all animecreators
 exports.animeCreator_list =(req,res)=>{
@@ -48,14 +49,68 @@ exports.animeCreator_detail =(req,res,next)=>{
     )
 };
 //display animecreator create form on GET
-exports.animeCreator_create_get =(req,res)=>{
-    res.send("NOT IMPEMENTENED: creator create GET");
+exports.animeCreator_create_get =(req,res,next)=>{
+    res.render("creator_form",{title:"Create AnimeCreator"});
 };
 
 //handle animecreator create on POST
-exports.animeCreator_create_post =(req,res)=>{
-    res.send("NOT IMPEMENTENED: creator create POST");
-};
+exports.animeCreator_create_post =[
+    // Validate and sanitize fields.
+    body("first_name")
+      .trim()
+      .isLength({ min: 1 })
+      .escape()
+      .withMessage("First name must be specified.")
+      .isAlphanumeric()
+      .withMessage("First name has non-alphanumeric characters."),
+    body("last_name")
+      .trim()
+      .isLength({ min: 1 })
+      .escape()
+      .withMessage("Last name must be specified.")
+      .isAlphanumeric()
+      .withMessage("Family name has non-alphanumeric characters."),
+    body("date_of_birth", "Invalid date of birth")
+      .optional({ checkFalsy: true })
+      .isISO8601()
+      .toDate(),
+    body("date_of_death", "Invalid date of death")
+      .optional({ checkFalsy: true })
+      .isISO8601()
+      .toDate(),
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+      // Extract the validation errors from a request.
+      const errors = validationResult(req);
+  
+      if (!errors.isEmpty()) {
+        // There are errors. Render form again with sanitized values/errors messages.
+        res.render("creator_form", {
+          title: "Create Author",
+          creator: req.body,
+          errors: errors.array(),
+        });
+        return;
+      }
+      // Data from form is valid.
+  
+      // Create an Author object with escaped and trimmed data.
+      const creator = new AnimeCreator({
+        first_name: req.body.first_name,
+        last_name: req.body.last_name,
+        date_of_birth: req.body.date_of_birth,
+        date_of_death: req.body.date_of_death,
+      });
+      creator.save((err) => {
+        if (err) {
+          return next(err);
+        }
+        // Successful - redirect to new author record.
+        res.redirect(creator.url);
+      });
+    },
+  ];
+  
 
 //display animecreator delete form on GET
 exports.animeCreator_delete_get =(req,res)=>{
