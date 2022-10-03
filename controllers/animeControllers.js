3a6036f38ad4c36dc1fa99a9a7ae4ceef2b6210e
ikram mohideen <ikram432k@gmail.com>
2022-10-03 18:54:29 +0530

    added form for creating new anime

--- controllers/animeControllers.js
+++ controllers/animeControllers.js
@@ -2,6 +2,7 @@ const Anime = require("../models/anime");
 const AnimeCreator = require("../models/animeCreator");
 const Genre = require("../models/genre");
 const Availabilty = require("../models/availability");
+const { body, validationResult } = require("express-validator");
 
 const async = require("async");
 
@@ -80,14 +81,116 @@ exports.anime_detail =(req,res,next)=>{
 };
 
 //display anime create form on GET
-exports.anime_create_get =(req,res)=>{
-    res.send("NOT IMPEMENTENED: anime create GET");
-};
+exports.anime_create_get =(req, res, next) => {
+    // Get all animeCreator and genres, which we can use for adding to our anime.
+    async.parallel(
+      {
+        animeCreators(callback) {
+            AnimeCreator.find(callback);
+        },
+        genres(callback) {
+            Genre.find(callback);
+        },
+      },
+      (err, results) => {
+        if (err) {
+          return next(err);
+        }
+        res.render("anime_form", {
+          title: "Create anime",
+          animeCreators: results.animeCreators,
+          genres: results.genres,
+        });
+      }
+    );
+  };
 
 //handle anime create on POST
-exports.anime_create_post =(req,res)=>{
-    res.send("NOT IMPEMENTENED: anime create POST");
-};
+exports.anime_create_post = [ 
+    // Convert the genre to an array.
+    (req, res, next) => {
+      if (!Array.isArray(req.body.genre)) {
+        req.body.genre =
+          typeof req.body.genre === "undefined" ? [] : [req.body.genre];
+      }
+      next();
+    },
+  
+    // Validate and sanitize fields.
+    body("title", "Title must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+    body("creator", "Creator must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+    body("summary", "Summary must not be empty.")
+      .trim()
+      .isLength({ min: 1 })
+      .escape(),
+    body("genre.*").escape(),
+  
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+      // Extract the validation errors from a request.
+      const errors = validationResult(req);
+  
+      // Create a anime object with escaped and trimmed data.
+      const anime = new Anime({
+        title: req.body.title,
+        creator: req.body.creator,
+        summary: req.body.summary,
+        genre: req.body.genre,
+      });
+  
+      if (!errors.isEmpty()) {
+        // There are errors. Render form again with sanitized values/error messages.
+  
+        // Get all animeCreator and genres for form.
+        async.parallel(
+          {
+            animeCreators(callback) {
+                AnimeCreator.find(callback);
+            },
+            genres(callback) {
+              Genre.find(callback);
+            },
+          },
+          (err, results) => {
+            if (err) {
+              return next(err);
+            }
+  
+            // Mark our selected genres as checked.
+            for (const genre of results.genres) {
+              if (anime.genre.includes(genre._id)) {
+                genre.checked = "true";
+              }
+            }
+            res.render("anime_form", {
+              title: "Create Anime",
+              animeCreators: results.animeCreators,
+              genres: results.genres,
+              anime,
+              errors: errors.array(),
+            });
+          }
+        );
+        return;
+      }
+  
+      // Data from form is valid. Save anime.
+      anime.save((err) => {
+        if (err) {
+          return next(err);
+        }
+        // Successful: redirect to new anime record.
+        res.redirect(anime.url);
+      });
+    },
+  ];
+
 
 //display anime delete form on GET
 exports.anime_delete_get =(req,res)=>{
