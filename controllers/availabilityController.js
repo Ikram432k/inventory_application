13ccb0099d3a87da8890a465fef5de1f8c0c3e3d
ikram mohideen <ikram432k@gmail.com>
2022-10-08 08:55:50 +0530

    updated contoller

--- controllers/availabilityController.js
+++ controllers/availabilityController.js
@@ -43,7 +43,7 @@ exports.availability_create_get =(req, res, next) => {
       }
       // Successful, so render.
       res.render("availability_form", {
-        title: "Create Availability",
+        title: "Create Telecast Details",
         anime_list: animes,
       });
     });
@@ -52,43 +52,42 @@ exports.availability_create_get =(req, res, next) => {
 
 //handle availability create on POST
 exports.availability_create_post = [
-  // Validate and sanitize fields.
-  body("anime", "Anime must be specified").trim().isLength({ min: 1 }).escape(),
-  body("studio", "studio must be specified")
-    .trim()
-    .isLength({ min: 1 })
-    .escape(),
-  body("status").escape(),
 
-  // Process request after validation and sanitization.
-  (req, res, next) => {
-    // Extract the validation errors from a request.
-    const errors = validationResult(req);
 
-    // Create a availability object with escaped and trimmed data.
-    const availability = new Availability({
-      anime: req.body.anime,
-      studio: req.body.studio,
-      status: req.body.status,
-    });
-
-    if (!errors.isEmpty()) {
-      // There are errors. Render form again with sanitized values and error messages.
-      Anime.find({}, "title").exec(function (err, animes) {
-        if (err) {
-          return next(err);
+    // Validate and sanitize fields.
+    body('anime', 'Name of anime must be specified').trim().isLength({ min: 1 }).escape(),
+    body('studio', 'studio name must be specified').trim().isLength({ min: 1 }).escape(),
+    body('status').escape(),
+    
+    
+    // Process request after validation and sanitization.
+    (req, res, next) => {
+  
+        // Extract the validation errors from a request.
+        const errors = validationResult(req);
+  
+        // Create a BookInstance object with escaped/trimmed data and current id.
+        var availability = new Availability(
+          { anime: req.body.anime,
+            studio: req.body.studio,
+            status: req.body.status,
+           });
+  
+        if (!errors.isEmpty()) {
+            // There are errors so render the form again, passing sanitized values and errors.
+            Anime.find({},'title')
+                .exec(function (err, animes) {
+                    if (err) { return next(err); }
+                    // Successful, so render.
+                    res.render('availability_form', { 
+                    title: 'Create Telecast Details', 
+                    anime_list : animes, 
+                    selected_anime : availability.anime._id , 
+                    errors: errors.array(),
+                    availability:availability });
+            });
+            return;
         }
-        // Successful, so render.
-        res.render("availability_form", {
-          title: "Create Availability",
-          anime_list: animes,
-          selected_book: availability.anime._id,
-          errors: errors.array(),
-          availability,
-        });
-      });
-      return;
-    }
 
     // Data from form is valid.
     availability.save((err) => {
@@ -167,7 +166,7 @@ exports.availability_update_get =function (req, res, next) {
       }
       // Success.
       res.render("availability_form", {
-        title: "Update  Availability",
+        title: "Update Telecast Detail",
         anime_list: results.animes,
         selected_anime: results.availability.anime._id,
         availability: results.availability,
@@ -207,7 +206,7 @@ exports.availability_update_post =[
                   if (err) { return next(err); }
                   // Successful, so render.
                   res.render('availability_form', { 
-                  title: 'Update Availability', 
+                  title: 'Update Telecast Detail', 
                   anime_list : animes, 
                   selected_anime : availability.anime._id , 
                   errors: errors.array(),
